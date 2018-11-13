b53f9b055887bacfbbc5d848d502ca1f281c615e
ilhwan <ikhwan62@naver.com>
2018-11-14 03:02:20 +0900

    my first commit

--- check.c
+++ check.c
@@ -1,15 +1,18 @@
 #include "myas.h"
 
-int is_valid(char *op, char *args)
-{
-	//printf("if valid, return 1\n");
-	//printf("otherwise, return 0\n");
-
-	/********************************/
-    /*  							*/
-	/*         syntax check         */
-    /*  							*/
-	/********************************/
-	/	modify		/	
-	return 1;
+int is_valid(char *op, char *args){
+	int i = 0,eval=0;
+	if(args[0] == '%'){
+		while(args[i] != ',') i++;
+		i++;
+		if(args[i] == '%') eval = 1;
+		else if(args[i] == '0') eval = 1;
+		i = 0;
+	}
+	else if(args[0]=='('||args[0]=='-'||args[0]=='0'||args[0]=='$'){
+		while(args[i] != ',') i++;
+		i++;
+		if(args[i] == '%') eval = 1;
+	}
+	return eval;
 }
