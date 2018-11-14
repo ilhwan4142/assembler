8ae68e27da65bd1315b35372282930a8502f8a32
ilhwan <ikhwan62@naver.com>
2018-11-14 11:36:59 +0900

    trans.c revised

--- trans.c
+++ trans.c
@@ -2,20 +2,71 @@
 
 int instr_trans(char *op, char *args, char* mcode)
 {
-	// check syntax 
+	int i = 0;
 	if(!is_valid(op, args)){
 		printf("Error: %s %s is not valid\n", op, args);
 		return 0;
 	}
 
-
-	strcpy(mcode, "AB CD EF");
-
-	/********************************/
-    /*  							*/
-	/*    generate machine code     */
-    /*  							*/
-	/********************************/
+	if(args[0] == '%'){
+		while (args[i] != ',') i++;
+		i++;
+		if(args[i] == '%') strcpy(mcode, "89");
+		else strcpy(mcode, "a3");
+		i=0;
+	}
+	else if(args[0] == '('||args[0] == '-') strcpy(mcode, "8b");
+	else if(args[0] == '0')	strcpy(mcode, "a1");
+	else if(args[0] == '$'){
+		while(args[i] != ',') i++;
+		i++;
+		while(args[i] != '\0'){
+			if(args[i] == 'a'){
+				strcpy(mcode, "b8");
+				i=0;
+				break;
+			}
+			else if(args[i] == 'b'){
+				i++;
+				if(args[i] == 'p'){
+					strcpy(mcode, "bd");
+					i=0;
+					break;
+				}
+				strcpy(mcode, "bb");
+				i=0;
+				break;
+			}
+			else if(args[i] == 'c'){
+				strcpy(mcode, "b9");
+				i=0;
+				break;
+			}
+			else if(args[i] == 'd'){
+				i++;
+				if(args[i] == 'i'){
+					strcpy(mcode, "bf");
+					i=0;
+					break;
+				}
+				strcpy(mcode, "ba");
+				i=0;
+				break;
+			}
+			else if(args[i] == 's'){
+				i++;
+				if(args[i] == 'p'){
+					strcpy(mcode, "bc");
+					i=0;
+					break;
+				}
+				strcpy(mcode, "be");
+				i=0;
+				break;
+			}
+				
+		i++;
+	}
 	
 	return 1;	
 }
