66a3fcedb4e737c91cd955c4a67db667ddc48dc9
ilhwan <ikhwan62@naver.com>
2018-11-14 12:08:43 +0900

    main.c revised

--- main.c
+++ main.c
@@ -27,7 +27,7 @@ int main(int argc, char* argv[])
 		// translate assembly into machine-code 
 		if(!instr_trans(op, args, mcode)){
 			printf("Error: %s %s cannot be translated\n", op, args);
-			continue;
+			strcpy(mcode, "error");
 		}
 		fprintf(ofp, "%s\n", mcode);
 	}
